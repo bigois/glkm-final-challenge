1ca8f7edb7f5f26f5b1f5fb1b86857a5a320697b
matheus-diefenthaeler <matheus.dieff@gmail.com>
2024-03-17 22:40:44 -0300

    wip: adicionando autenticacao nos endpoints pro gateway

---
+++ Gateway/src/main/java/com/fiap/tech/Gateway/config/SecurityFilter.java
@@ -0,0 +1,42 @@
+package com.fiap.tech.Gateway.config;
+
+import com.fiap.tech.Gateway.services.ProductGatewayService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    ProductGatewayService productGatewayService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if (token != null) {
+            try {
+                String tokenn = request.getHeader("Authorization");
+                Boolean authenticate = productGatewayService.authenticate(tokenn);
+                if (authenticate)
+                    filterChain.doFilter(request, response);
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+//        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
