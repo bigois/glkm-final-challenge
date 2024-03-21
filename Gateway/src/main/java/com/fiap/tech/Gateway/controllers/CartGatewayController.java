f8e42ab781c7e50076baf85e75e6cbdc832feb60
bigois <guilhermebigois@outlook.com>
2024-03-21 19:25:11 -0300

    feat: add token

--- Gateway/src/main/java/com/fiap/tech/Gateway/controllers/CartGatewayController.java
+++ Gateway/src/main/java/com/fiap/tech/Gateway/controllers/CartGatewayController.java
@@ -1,10 +1,16 @@
 package com.fiap.tech.Gateway.controllers;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fiap.tech.Gateway.dtos.CartAddRequestDTO;
 import com.fiap.tech.Gateway.dtos.CartAddResponseDTO;
 import com.fiap.tech.Gateway.dtos.CartCreateRequestDTO;
 import com.fiap.tech.Gateway.dtos.CartCreateResponseDTO;
 import com.fiap.tech.Gateway.services.CartGatewayService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -33,8 +39,25 @@ public class CartGatewayController {
     }
 
     @PostMapping
-    public ResponseEntity<Mono<CartCreateResponseDTO>> createCart(@RequestBody @Valid CartCreateRequestDTO cartCreateRequestDTO){
-        return ResponseEntity.status(HttpStatus.CREATED).body(cartGatewayService.createCart(cartCreateRequestDTO));
+    public ResponseEntity<Mono<CartCreateResponseDTO>> createCart(@RequestBody @Valid CartCreateRequestDTO cartCreateRequestDTO, HttpServletRequest request) {
+        String jwtToken = request.getHeader("Authorization").replace("Token: ", "");
+
+        Algorithm algorithm = Algorithm.HMAC256("baeldung");
+        JWTVerifier verifier = JWT.require(algorithm)
+                .withIssuer("Baeldung") // TODO: Token certo aqui
+                .build();
+
+        DecodedJWT decodedJWT = null;
+        try {
+            decodedJWT = verifier.verify(jwtToken);
+        } catch (JWTVerificationException e) {
+            System.out.println(e.getMessage());
+        }
+
+        UUID userId = UUID.fromString(decodedJWT.getClaim("userId").asString());
+        CartCreateRequestDTO cartCreateRequestDTOWithUserId = new CartCreateRequestDTO(userId, cartCreateRequestDTO.idProduct(), cartCreateRequestDTO.price(), cartCreateRequestDTO.quantity());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(cartGatewayService.createCart(cartCreateRequestDTOWithUserId));
     }
 
     @PostMapping(path = "/add-item-cart/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
